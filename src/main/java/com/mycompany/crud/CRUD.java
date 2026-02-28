9cfc30f87c2547bf6b0414c7b1dc3e071a3cc616
cielo2307 <rodriguezcielo2307@gmail.com.com>
2026-02-28 11:18:00 -0500

    actualizar por clases

--- src/main/java/com/mycompany/crud/CRUD.java
+++ src/main/java/com/mycompany/crud/CRUD.java
@@ -11,7 +11,7 @@ public class CRUD {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        
 
         try {
 
@@ -22,32 +22,51 @@ public class CRUD {
 
             Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
 
-            System.out.println("Eliminar Usuario");
-            System.out.print("Ingrese el idUsuario: ");
-            int idUsuarios = scanner.nextInt();
+            deleteUser(conexion);
             
-            // Sentencia DELETE
-            String sql = "DELETE FROM usuarios WHERE idUsuario = ?";
-            PreparedStatement consulta = conexion.prepareStatement(sql);
-
-            consulta.setInt(1, idUsuarios);
-
-            int filas = consulta.executeUpdate();
-
-            if (filas > 0) {
-                System.out.println("Usuario eliminado");
-            } else {
-                System.out.println("No existe un usuario con ese id");
-            }
-
-            consulta.close();
-            conexion.close();
 
         }
         catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
-        scanner.close();
+        //scanner.close();
     }
+    
+    public static void createUser(){
+        
+    }
+    
+    public static void readUser(){
+        
+    }
+    
+    public static void updateUser(){
+        
+    }
+    
+    public static void deleteUser(Connection conexion) throws SQLException{
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Eliminar Usuario");
+        System.out.print("Ingrese el idUsuario: ");
+        int idUsuarios = scanner.nextInt();
+
+        // Sentencia DELETE
+        String sql = "DELETE FROM usuarios WHERE idUsuario = ?";
+        try (PreparedStatement consulta = conexion.prepareStatement(sql)) {
+            consulta.setInt(1, idUsuarios);
+            int filas = consulta.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Usuario eliminado");
+            } else {
+                System.out.println("No existe un usuario con ese id");
+            }
+            //conexion.close();
+        }
+    }
+    
+    
+    
+    
 }
