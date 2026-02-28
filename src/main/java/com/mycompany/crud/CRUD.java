2929bc503af8d7309d3497839a6e284f4ddbcd05
cielo2307 <rodriguezcielo2307@gmail.com.com>
2026-02-28 12:10:30 -0500

    restablecer codigo

--- src/main/java/com/mycompany/crud/CRUD.java
+++ src/main/java/com/mycompany/crud/CRUD.java
@@ -11,43 +11,62 @@ public class CRUD {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        
 
         try {
 
             // Conexión a la base de datos
-            String url = "jdbc:mysql://localhost:3306/e-form";
+            String url = "jdbc:mysql://formacionsena.c9bfqfswwhqk.us-east-1.rds.amazonaws.com:3306/eform";
             String usuario = "root";
-            String contraseña = "cielo";
+            String contraseña = "8Fg2Rc7hHChxibkbqjSI";
 
             Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
 
-            System.out.println("Eliminar Usuario");
-            System.out.print("Ingrese el idUsuario: ");
-            int idUsuarios = scanner.nextInt();
+            deleteUser(conexion);
             
-            // Sentencia DELETE
-            String sql = "DELETE FROM usuarios WHERE idUsuario = ?";
-            PreparedStatement consulta = conexion.prepareStatement(sql);
-
-            consulta.setInt(1, idUsuarios);
-
-            int filas = consulta.executeUpdate();
-
-            if (filas > 0) {
-                System.out.println("Usuario eliminado");
-            } else {
-                System.out.println("No existe un usuario con ese id");
-            }
-
-            consulta.close();
-            conexion.close();
 
         }
         catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
-        scanner.close();
+        //scanner.close();
     }
+    
+    public static void createUser(){
+        
+    }
+    
+    public static void readUser(){
+        
+    }
+    
+    public static void updateUser(){
+        
+    }
+    
+    public static void deleteUser(Connection conexion) throws SQLException{
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Eliminar Usuario");
+        System.out.print("Ingrese el idUsuario: ");
+        int idUsuarios = scanner.nextInt();
+
+        // Sentencia DELETE
+        String sql = "DELETE FROM usuarios WHERE idUsuario = ?";
+        try (PreparedStatement consulta = conexion.prepareStatement(sql)) {
+            consulta.setInt(1, idUsuarios);
+            int filas = consulta.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Usuario eliminado");
+            } else {
+                System.out.println("No existe un usuario con ese id");
+            }
+            //conexion.close();
+        }
+    }
+    
+    
+    
+    
 }
