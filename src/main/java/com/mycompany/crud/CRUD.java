7bb78bd042258131a194c6c3233ba47121084407
cielo2307 <rodriguezcielo2307@gmail.com.com>
2026-02-28 08:31:56 -0500

    Conexión a la base de datos

--- src/main/java/com/mycompany/crud/CRUD.java
+++ src/main/java/com/mycompany/crud/CRUD.java
@@ -13,37 +13,7 @@ public class CRUD {
 
         Scanner scanner = new Scanner(System.in);
 
-        try {
-
-            // Conexión a la base de datos
-            String url = "jdbc:mysql://localhost:3306/e-form";
-            String usuario = "root";
-            String contraseña = "cielo";
-
-            Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
-
-            System.out.println("Eliminar Usuario");
-            System.out.print("Ingrese el idUsuario: ");
-            int idUsuarios = scanner.nextInt();
-
-            // Sentencia DELETE
-            String sql = "DELETE FROM usuarios WHERE idUsuario = ?";
-            PreparedStatement consulta = conexion.prepareStatement(sql);
-
-            consulta.setInt(1, idUsuarios);
-
-            int filas = consulta.executeUpdate();
-
-            if (filas > 0) {
-                System.out.println("Usuario eliminado");
-            } else {
-                System.out.println("No existe un usuario con ese id");
-            }
-
-            consulta.close();
-            conexion.close();
-
-        } catch (SQLException e) {
+         catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
